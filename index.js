8d4fe8bfc55c0c4deefda695a19e76ea2525efd8
YadavAnuj90 <163463130+YadavAnuj90@users.noreply.github.com>
2025-03-15 19:55:00 +0530

    add blog upload feature

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+
+const Blog = require("./models/blog");
+
 const PORT = 8000;
 
 app.set("view engine", "ejs");
@@ -20,10 +23,13 @@ mongoose.connect("mongodb://localhost:27017/blogify").then(() => console.log("Co
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+   const allBlogs = await Blog.find();
   res.render("home" , {
     user: req.user,
+    blogs: allBlogs
   });
 });
 
--- routes/blog.js
+++ routes/blog.js
@@ -1,34 +1,39 @@
-
 const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
 
+const Blog = require("../models/blog");
+
 const router = Router();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.resolve(`./public/uploads/`));
-    },
-    filename: function (req, file, cb) {
-        const fileName = `${Date.now()}-${file.originalname}`;
-        cb(null, fileName);
-    }
-  })
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`));
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
 
-  const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
+router.get("/add-new", (req, res) => {
+  return res.render("addBlog", {
+    user: req.user,
+  });
+});
 
-router.get("/add-new", (req,res) => {
-    return res.render("addBlog", {
-        user: req.user,
-    })
-})
+router.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, body } = req.body;
+  const newBlog = await Blog.create({
+    title,
+    body,
+    coverImageURL: `/uploads/${req.file.filename}`,
+    createdBy: req.user._id,
+  });
 
-router.post("/", upload.single("coverImage"), (req,res) => {
-    console.log(req.body);
-    console.log(req.file);
-    return res.redirect("/");
-})
-
+  return res.redirect(`/blog/${newBlog._id}`);
+});
 
 module.exports = router;
